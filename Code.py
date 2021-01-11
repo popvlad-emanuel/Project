d437450f2c75ebe9e3f679d2f283dda28c696121
popvlad-emanuel <73606661+popvlad-emanuel@users.noreply.github.com>
2021-01-11 18:36:16 +0200

    Add files via upload

---
+++ Code.py
@@ -0,0 +1,2 @@
+import subprocess
+subprocess.run(['/home/vladp/Desktop/Sniper/sniper/benchmarks/run-sniper', '--benchmarks=splash2-fft-test-4','-c/home/vladp/Desktop/Sniper/sniper/benchmarks/gainestown'])
